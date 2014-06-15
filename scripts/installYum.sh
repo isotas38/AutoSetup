cc22b958d179c42da9737ec5b17d5dcf8f4778e7
isotas38 <isotas38@example.com>
2014-06-15 23:53:15 +0900

    add developer library to yum install list

--- scripts/installYum.sh
+++ scripts/installYum.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 yum -y install yum-plugin-fastestmirror
 yum -y update
-yum -y install ntp man tree vim-enhanced unzip wget gcc gcc-c++ make git zsh patch sudo
+yum -y install ntp man tree unzip wget gcc gcc-c++ make git zsh patch sudo procps autoconf
+yum -y install openssl-devel readline-devel gdbm-devel ncurses-devel db4-devel libffi-devel libyaml-devel tk-devel
