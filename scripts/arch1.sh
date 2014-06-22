b2878ff3dce3558f7c9100c92cde10c3727168f9
isotas38 <isotas38@example.com>
2014-06-22 13:27:53 +0900

    fix problem of environment variable

--- scripts/arch1.sh
+++ scripts/arch1.sh
@@ -7,7 +7,7 @@ sgdisk --zap-all /dev/sda
 sfdisk /dev/sda < $CURRENT/../conf/arch.layout
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-sed -i “1i Server=http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/$repo/os/$arch
+sed -i '1i Server=http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/\$repo/os/\$arch'
 pacstrap -i /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
 cp $CURRENT/arch2.sh /mnt/root
