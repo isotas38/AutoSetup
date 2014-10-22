f0a5c8a0702b68ac1e4723017207b5971983bff5
isotas38 <isotas38@example.com>
2014-10-22 20:42:07 +0900

    modify yum install packages

--- scripts/installYum.sh
+++ scripts/installYum.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 yum -y install yum-plugin-fastestmirror
 yum -y update
-yum -y install ntp man tree unzip wget gcc gcc-c++ make git zsh patch sudo procps autoconf
-yum -y install openssl-devel readline-devel gdbm-devel ncurses-devel db4-devel libffi-devel libyaml-devel tk-devel
+yum -y groupinstall 'Development Tools'
+yum -y install ImageMagick autoconf curl gcc gcc-c++ git ipa-pgothic-fonts m4 make man ntp patch procps sudo texinfo tree unzip wget
+yum -y install ImageMagick-devel bzip2-devel curl-devel db4-devel expat-devel gdbm-devel libffi-devel libxml2-devel libxslt-devel libyaml-devel ncurses-devel openssl-devel readline-devel tk-devel zlib-devel
